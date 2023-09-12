7556d769abbad7c38b43a94d1c244ac59b4d58a9
Michael Mar <michaelmar719@gmail.com>
2023-09-12 22:59:22 +1200

    created react app and started stuff

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React, {useState} from "react";
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+import Home from "./Routes/Home";
+import Rubric from "./Routes/Rubric";
+import Marking from "./Routes/Marking";
+
+function App() {
+    return (
+        <Router>
+            <nav>This is a nav</nav>
+            <Routes>
+                <Route path="/" exact element={<Home/>} />
+                <Route path="/rubric" element={<Rubric/>} />
+                <Route path="/marking" element={<Marking/>} />
+            </Routes>
+        </Router>
+    )
+}
+
+export default App;
