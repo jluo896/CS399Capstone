6754b9468040e316e0daa9830abb31d89f4e0300
Michael Mar <michaelmar719@gmail.com>
2023-09-15 22:04:20 +1200

    no minor changes on backend

---
+++ backend/controllers/rubricController.js
@@ -0,0 +1,9 @@
+require("file-saver");
+const Blob = require("buffer").Blob;
+
+const router = require("express").Router();
+
+//router.route("").get((req, res) => {
+//})
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// controllers
+const rubricController = require("./controllers/rubricController")
+
+app.use("/rubric", rubricController);
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
