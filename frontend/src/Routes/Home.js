10e821231dcff89be1089d3833c570248d791468
Michael Mar <michaelmar719@gmail.com>
2023-09-21 18:37:03 +1200

    fixed links

--- frontend/src/Routes/Home.js
+++ frontend/src/Routes/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
         <div class = "home">
             <div><Link to="/rubric" class = "front" type = "botton">Rubric</Link></div>
             <div><Link to="/upload" class = "front">Upload</Link></div>
-            <div><Link to="/grading" class = "front">Grading</Link></div>
+            <div><Link to="/grading/menu" class = "front">Grading</Link></div>
         </div>
     )
 };
--- frontend/src/Routes/Upload.js
+++ frontend/src/Routes/Upload.js
@@ -81,7 +81,7 @@ export default function Upload() {
                 <br/>
                 <button onClick={onFilesUpload}>Upload</button>
             </div>
-            <div><Link to="/grading">Grade Now</Link></div>
+            <div><Link to="/grading/menu">Grade Now</Link></div>
         </div>
     )
     
