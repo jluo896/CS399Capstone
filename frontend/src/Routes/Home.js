1ba6b37c1ca0d067c29e06154562317e266c1fce
zxu703 <zxu703@aucklanduni.ac.nz>
2023-09-23 12:36:38 +0800

    css
    1

--- frontend/src/Routes/Home.js
+++ frontend/src/Routes/Home.js
@@ -5,9 +5,9 @@ import {Link} from "react-router-dom";
 export default function Home() {
     return (
         <div class = "home">
-            <div class = "div-button-l"><Link to="/rubric" class = "button-link" type = "botton">Rubric</Link></div>
-            <div class = "div-button-c"><Link to="/upload" class = "button-link" type = "botton">Upload</Link></div>
-            <div class = "div-button-r"><Link to="/grading/menu" class = "button-link" type = "botton">Grading</Link></div>
+            <div class = "div-button-l move-to-c"><Link to="/rubric" class = "button-link" type = "botton">Rubric</Link></div>
+            <div class = "div-button-c move-to-c"><Link to="/upload" class = "button-link" type = "botton">Upload</Link></div>
+            <div class = "div-button-r move-to-c"><Link to="/grading/menu" class = "button-link" type = "botton">Grading</Link></div>
         </div>
     )
 };
