40412565636a6b2677759fe9985ca47158fe6c61
jluo896 <90538669+jluo896@users.noreply.github.com>
2023-09-30 14:29:00 +1300

    for v2 rubric

--- frontend/src/Routes/GradePage1.js
+++ frontend/src/Routes/GradePage1.js
@@ -7,17 +7,67 @@ export default function GradePage1() {
     // grading functions
     const rubrics = [
         {
-            "title": "question 1",
-            "description": "this is a description",
-            "marks": [0, 1, 2, "", ""],
-            "comments": ["bad", "good"]
+            "type": "radio-group",
+            "required": false,
+            "label": "Question 1",
+            "inline": false,
+            "name": "radio-group-1695435674642-0",
+            "access": false,
+            "other": false,
+            "values": [
+                {
+                    "label": "0",
+                    "value": "comment 1",
+                    "selected": false
+                },
+                {
+                    "label": "1",
+                    "value": "comment 2",
+                    "selected": false
+                },
+                {
+                    "label": "2",
+                    "value": "comment 3",
+                    "selected": false
+                },
+                {
+                    "label": "",
+                    "value": "comment 4",
+                    "selected": false
+                }
+            ]
         },
         {
-            "title": "question 2",
-            "description": "this is another description",
-            "marks": [0, 1, 2, 3],
-            "comments": ["real reaaal reeeeeeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeaally long txt.", "good"]
-        },
+            "type": "select",
+            "required": false,
+            "label": "Question 2",
+            "className": "form-control",
+            "name": "select-1695435675690-0",
+            "access": false,
+            "multiple": false,
+            "values": [
+                {
+                    "label": "0",
+                    "value": "comment 1",
+                    "selected": true
+                },
+                {
+                    "label": "2",
+                    "value": "comment 2",
+                    "selected": false
+                },
+                {
+                    "label": "4",
+                    "value": "",
+                    "selected": false
+                },
+                {
+                    "label": "6",
+                    "value": "",
+                    "selected": false
+                }
+            ]
+        }
     ]
     const studentInfo = [
         {
@@ -37,55 +87,46 @@ export default function GradePage1() {
         <div>
             <Link to="/">Back</Link>
             <div class="blank-container">
-                {
-                    <div class="grade-header">
-                        <div class="grade-title">
-                            <p>Course Code: {studentInfo[0].courseCode}</p>
-                            <p>Assignment: {studentInfo[0].assignmenttitle}</p>
-                        </div>
-                        <div class="grade-details">
-                            <p>Student Name: {studentInfo[0].stdname}</p>
-                            <p>Student Id: {studentInfo[0].stdid}</p>
-                        </div>
-                    </div>}
-                <div>
-                    <ul>
-                        {rubrics.map(value =>
-                            <div class="question">
-                                <label>{value.title}</label>
-                                <p>{value.description}</p>
-                                {value.marks.map(mark => mark !== "" ?
-                                    <span>
-                                        <input type="radio" name={value.title} value={mark} />
-                                        <label>{mark}</label>
-                                    </span> : <span></span>)}
-                                {/*<div>*/}
-                                {/*    <input type="radio" value="none" id="radio_1" name={value.title} />*/}
-                                {/*</div>*/}
-                                <input class="question-numberinput" type="text" name={value.title} placeholder={'Other'} />
-                                <br />
-                                <line></line>
-                                {value.comments.map(mark =>
-
-                                    <div>
-                                        <input type="radio" name={value.title + "comment"} value={mark} />
-                                        <label class="radio-comments">{mark}</label>
-                                    </div>
-                                )}
-                                <textarea type="text" name={value.title} placeholder={'Other'} />
-
-                            </div>)}
-
-                    </ul>
-                    <div class="btn-block">
-                        <button type="submit" href="/"><Link to="/grading/menuN" type="botton">Commit Change</Link> </button>
-                    </div>
-                    <div class="btn-block">
-
-                        <button type="button" onClick={showPopup}>Check</button>
-                    </div>
-
+                <div class="grade-title">
+                    <p>Course Code: {studentInfo[0].courseCode}</p>
+                    <p>Assignment: {studentInfo[0].assignmenttitle}</p>
                 </div>
+                <div class="grade-details">
+                    <p>Student Name: {studentInfo[0].stdname}</p>
+                    <p>Student Id: {studentInfo[0].stdid}</p>
+                </div>
+                {rubrics.map(value => 
+                    <div class="question">
+                        <label>{value.label}</label>
+                        <label>placeholder for description</label>
+                        <div class="question-radioBox">
+                            {value.values.map(mark => mark.label !== "" ?
+                            <span>
+                                <input type="radio" name={mark} value={mark.label} />
+                                <label>{mark.label}</label>
+                               </span> : <span></span>
+                            )}
+                            <div class="grade-other">
+                                <label>other</label>
+                                <input class="question-numberinput" type="text" name={value.title} placeholder="other"/>
+                            </div>
+                        </div>
+                        <div class="question-radioBox">
+                            {value.values.map(mark => mark.value !== "" ?
+                                <span>
+                                    <input type="radio" name={mark} value={mark.value} />
+                                    <label>{mark.value}</label>
+                                </span> : <span></span>
+                                                    )}
+                                    <textarea type="text" name={value.title} placeholder="other" />
+                        </div>
+
+                    </div> )}
+                    <div class="btn-block">
+                        <button class="question-buttons" type="button" href="/"><Link to="/grading/menuN" class="button-link" type="botton">Commit Change</Link></button>
+                        <button class="question-buttons" type="button" onClick={showPopup}>Check</button>
+                    </div>
+
             </div>
         </div>
     )
